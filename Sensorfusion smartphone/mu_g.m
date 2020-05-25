ccea78a4bd8b75f84e6a1f839854ebf9ba23e927
Jonatan Nordh <Jonatan@Karolinas-MacBook-Pro-2.local>
2020-05-25 10:18:20 +0200

    Task 4-6

---
+++ Sensorfusion smartphone/mu_g.m
@@ -0,0 +1,14 @@
+function [x, P] = mu_g(x, P, yacc, Ra, g0)
+   
+    h = Qq(x)*g0;
+    [Q0, Q1, Q2, Q3] = dQqdq(x);
+    H = [Q0'*g0, Q1'*g0, Q2'*g0, Q3'*g0];
+    
+    S = H*P*H' + Ra;
+    K = P*H'*inv(S);
+    
+    x = x + K*(yacc-h);
+    P = P - K*S*K';
+
+end
+
--- Sensorfusion smartphone/tu_qw_no_omega.m
+++ Sensorfusion smartphone/tu_qw_no_omega.m
@@ -1,12 +1,10 @@
 function [x, P] = tu_qw_no_omega(x, P, T, Rw)
     %No Omega
-    alpha = 2*acos(x(1));
-
     F = eye(4);
     G = 0.5 * Sq(x) * T;
-    A = F + G*[0 1 1 1]*(1/sin(0.5*alpha));
     
-    x = A*x;
-    P = A*P*A' + Rw;
+    x = F*x;
+    P = F*P*F' + G*Rw*G';
+
 end
 
